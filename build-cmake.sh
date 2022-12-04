44d5fbe43fa24288817e5c2a568639454ceab099
Yurii Cherkasov <strategarius@protonmail.com>
2022-12-04 05:36:53 +0000

    Config cmake

--- build-cmake.sh
+++ build-cmake.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 rm -rf build-cmake
 mkdir build-cmake && cd build-cmake
-# /Users/atatat/Qt/6.4.1/macos
-# /usr/local/Cellar/qt/6.4.0/lib/cmake
-# /Users/atatat/Qt/QtDesignStudio/qt5_design_studio_reduced_version/lib/cmake/Qt5Widgets
-cmake .. -DCMAKE_PREFIX_PATH="/Users/atatat/Qt/6.4.1/macos" -DQt5Widgets_DIR=/Users/atatat/Qt/QtDesignStudio/qt5_design_studio_reduced_version/lib/cmake/Qt5Widgets
+cmake .. -DCMAKE_PREFIX_PATH="/Users/atatat/Qt/6.4.1/macos" -DQt5Widgets_DIR="~/Qt/QtDesignStudio/qt5_design_studio_reduced_version/lib/cmake/Qt5Widgets"
